8a639617a1f2e070f3a8eae8cbe23a59040c1725
richard <rldarklight@gmail.com>
2023-02-01 21:54:51 +0800

    final

--- .github/workflows/uploadGdrive.py
+++ .github/workflows/uploadGdrive.py
@@ -7,29 +7,22 @@ print(token)
 headers = {
     "Authorization": "Bearer {}".format(token)
 }
-# params = {
-#     "name": "test_apk",
-#     "mimeType": "application/vnd.android.package-archive"
-# }
-# files = {
-#     "data": ("metadata", json.dumps(params), "application/json;charset=UTF-8"),
-#     "file": open("build/app/outputs/flutter-apk/app-release.apk", "rb")
-# }
 params = {
-    "name": "testtttt",
-    "mimeType": "plain/text"
+    "name": "test_apk",
+    "mimeType": "application/vnd.android.package-archive"
 }
 files = {
     "data": ("metadata", json.dumps(params), "application/json;charset=UTF-8"),
-    "file": open(".github/workflows/test.txt", "rb")
+    "file": open("build/app/outputs/flutter-apk/app-release.apk", "rb")
 }
+
 response = requests.post(
     "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart", headers=headers, files=files)
 
 
 json_data = json.loads(response.text)
-# link = 'https://drive.google.com/file/d/{}/view'.format(json_data["id"])
+link = 'https://drive.google.com/file/d/{}/view'.format(json_data["id"])
 print(json_data["id"])
 
-# with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-#     print(f'link={link}', file=fh)
+with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'link={link}', file=fh)
