b70911b64347d6242e9f4ffbb8db17c538a1705a
richard <rldarklight@gmail.com>
2023-02-01 19:14:58 +0800

    build

--- .github/workflows/uploadGdrive.py
+++ .github/workflows/uploadGdrive.py
@@ -1,8 +1,10 @@
 import json
 import requests
+import os
 
+token = os.environ.get("SLACK_BOT_TOKEN")
 headers = {
-    "Authorization": "Bearer ya29.a0AVvZVsp96EcPXS2tNEym45jPROX9dZiZnZrDernfk5eFm3bpHRzt_7a6Ybwdwjgsic4Q5qMhb5Vw4Mc9ZPTYV1wvgNmJ0uVTy5xUPJp6QDOrNERBEY19IL9GTvZdFC0nF65-TpDsiohczro1eRUaDidFHXPYaCgYKAbsSARISFQGbdwaIfTw4I6gcnzd9itcs8U3YNg0163"
+    "Authorization": "Bearer {}".format(token)
 }
 params = {
     "name": "test_apk",
